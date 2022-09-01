3eb0b121ce392e79d7d75d5d59e8b78f6f94d7d9
Hugo Gibbs <hugo.d.gibbs@gmail.com>
2022-09-01 01:29:19 -0400

    .

--- java.js
+++ java.js
@@ -1,103 +1,22 @@
 
-// row 1 is inside the container and has 4 squared inside
 
-const row1 = document.createElement('div');
-row1.classList.add('row1');
-container.appendChild(row1);
+for (let i = 0; i < 256; i++){
 
-const square1 = document.createElement('div');
-square1.classList.add('square1');
-square1.textContent = '1';
-row1.appendChild(square1);
+const square = document.createElement('div');
+square.className = 'square';
+square.id = 'square';
+document.getElementById('container').appendChild(square);
 
-const square2 = document.createElement('div');
-square2.classList.add('square2');
-square2.textContent = '2';
-row1.appendChild(square2);
+}
 
-const square3 = document.createElement('div');
-square3.classList.add('square3');
-square3.textContent = '3';
-row1.appendChild(square3);
+const square = document.querySelector('square');
 
-const square4 = document.createElement('div');
-square4.classList.add('square4');
-square4.textContent = '4';
-row1.appendChild(square4);
+container.addEventListener('mouseover', e => {
+   e.target.style.background = 'black';
+})
 
-// row 2 is inside the container and has 4 squared inside
+const button = document.querySelector('button');
 
-const row2 = document.createElement('div');
-row2.classList.add('row2');
-container.appendChild(row2);
-
-const square5 = document.createElement('div');
-square5.classList.add('square5');
-square5.textContent = '5';
-row2.appendChild(square5);
-
-const square6 = document.createElement('div');
-square6.classList.add('square6');
-square6.textContent = '6';
-row2.appendChild(square6);
-
-const square7 = document.createElement('div');
-square7.classList.add('square7');
-square7.textContent = '7';
-row2.appendChild(square7);
-
-const square8 = document.createElement('div');
-square8.classList.add('square8');
-square8.textContent = '8';
-row2.appendChild(square8);
-
-// row 3 is inside the container and has 4 squared inside
-const row3 = document.createElement('div');
-row3.classList.add('row3');
-container.appendChild(row3);
-
-const square9 = document.createElement('div');
-square9.classList.add('square9');
-square9.textContent = '9';
-row3.appendChild(square9);
-
-const square10 = document.createElement('div');
-square10.classList.add('square10');
-square10.textContent = '10';
-row3.appendChild(square10);
-
-const square11 = document.createElement('div');
-square11.classList.add('square11');
-square11.textContent = '11';
-row3.appendChild(square11);
-
-const square12 = document.createElement('div');
-square12.classList.add('square12');
-square12.textContent = '12';
-row3.appendChild(square12);
-
-
-// row 4 is inside the container and has 4 squared inside
-const row4 = document.createElement('div');
-row4.classList.add('row4');
-container.appendChild(row4);
-
-const square13 = document.createElement('div');
-square13.classList.add('square13');
-square13.textContent = '13';
-row4.appendChild(square13);
-
-const square14 = document.createElement('div');
-square14.classList.add('square14');
-square14.textContent = '14';
-row4.appendChild(square14);
-
-const square15 = document.createElement('div');
-square15.classList.add('square15');
-square15.textContent = '15';
-row4.appendChild(square15);
-
-const square16 = document.createElement('div');
-square16.classList.add('square16');
-square16.textContent = '16';
-row4.appendChild(square16);
+document.addEventListener('click', e => {
+    let grid = prompt('Insert the number of grids:');
+})
